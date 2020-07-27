1f1d61f3941a6c1403bf70b419fc9f37e84ed8da
Harshit Agarwal <haagar@microsoft.com>
2020-07-27 13:41:59 +0530

    Removing console log and updating README

--- src/component1.tsx
+++ src/component1.tsx
@@ -3,7 +3,6 @@ import { AzureAD } from 'react-aad-msal';
 import { authProvider as authProvider1 } from './authProvider1';
 
 const Component1 = (): JSX.Element => {
-    console.log('component1');
     return (
         <AzureAD provider={authProvider1} forceLogin={true}>
             <p>this is component 1</p>
--- src/component2.tsx
+++ src/component2.tsx
@@ -3,13 +3,11 @@ import { AzureAD } from 'react-aad-msal';
 import { authProvider as authProvider2 } from './authProvider2';
 
 const Component2 = (): JSX.Element => {
-    console.log('component2');
     return (
         <AzureAD provider={authProvider2} forceLogin={true}>
             <p>this is component 2</p>
         </AzureAD>
     );
-    console.log('efefef');
 };
 
 export default Component2;
